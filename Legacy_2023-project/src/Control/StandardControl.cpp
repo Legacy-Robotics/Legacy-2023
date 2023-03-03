5a688b1b505e231c5494768258cf46db30dd6950
BrandonComins <brandcomin@gmail.com>
2023-03-03 11:28:44 -0800

    add 'standard-bot' control

---
+++ Legacy_2023-project/src/Control/StandardControl.cpp
@@ -0,0 +1,87 @@
+#ifdef TARGET_STANDARD
+
+#include "drivers.hpp"
+#include "drivers_singleton.hpp"
+//
+#include "tap/control/command_mapper.hpp"
+#include "tap/control/hold_command_mapping.hpp"
+#include "tap/control/hold_repeat_command_mapping.hpp"
+#include "tap/control/press_command_mapping.hpp"
+#include "tap/control/setpoint/commands/calibrate_command.hpp"
+#include "tap/control/toggle_command_mapping.hpp"
+//include subsystems below
+#include "../Subsystems/DriveTrain.hpp"
+//include commands below
+#include "../Subsystems/OmniDrive.hpp"
+
+/*
+ * NOTE: We are using the DoNotUse_getDrivers() function here
+ *      because this file defines all subsystems and command
+ *      and thus we must pass in the single statically allocated
+ *      Drivers class to all of these objects.
+ */
+src::driversFunc drivers = src::DoNotUse_getDrivers;
+
+using namespace tap;
+using namespace tap::control;
+
+//include subsystem namespaces below
+using namespace src::Drivetrain;
+
+namespace StandardControl {
+
+// Define Subsystems Here
+DriveTrainSubsystem drive_train(drivers());
+// Define Commands Here
+OmniDrive omni_drive(drivers(), &drive_train);
+
+// Define Command Mappings Here
+HoldCommandMapping leftSwitchUp(
+    drivers(), 
+    {&omni_drive}, 
+    RemoteMapState(Remote::Switch::LEFT_SWITCH, Remote::SwitchState::UP));
+// Register Subsystems with drivers->commandScheduler.registerSubsystem(&subsystem_name);
+void registerSubsystems(src::Drivers* drivers)
+{
+    drivers->commandScheduler.registerSubsystem(&drive_train);
+}
+
+// Initialize Subsystems with subsystem.initialize();
+void initializeSubsystems()
+{
+    drivetrain.initialize();
+}
+
+// Set Default Command with subsystem.setDefaultCommand(&command)
+void setDefaultCommands(src::Drivers* drivers)
+{
+    drivetrain.setDefaultCommand(&omni_drive);
+}
+
+// Set Commands scheduled on startup
+void startupCommands(src::Drivers* drivers)
+{
+
+}
+
+// Register IO Mappings with drivers->commandMapper.addMap(&commandMapping)
+void registerIOMappings(src::Drivers* drivers)
+{
+    drivers->commandMapper.addMap(&leftSwitchUp);
+}
+
+} //namespace StandardControl
+
+namespace src::Control
+{
+    void initializeSubsystemCommands(src::Drivers* drivers)
+    {
+        StandardControl::initializeSubsystems();
+        StandardControl::registerSubsystems(drivers);
+        StandardControl::setDefaultCommands(drivers);
+        StandardControl::startupCommands(drivers);
+        StandardControl::registerIOMappings(drivers);
+    }
+} //namespace src::Control
+
+#endif /*TARGET_STANDARD*/
