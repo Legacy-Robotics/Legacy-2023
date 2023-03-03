0189496cdc8289fb1ffbb392788a25be0bf03ac2
BrandonComins <brandcomin@gmail.com>
2023-03-03 10:52:42 -0800

    ran rename template script

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'taproot-template-project'
-copyright = '2020, taproot-template-project'
-author = 'taproot-template-project'
+project = 'Legacy_2023'
+copyright = '2020, Legacy_2023'
+author = 'Legacy_2023'
 
 # The full version, including alpha/beta/rc tags
 release = '1.0.0'
@@ -55,7 +55,7 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 
-breathe_default_project = "taproot-template-project"
+breathe_default_project = "Legacy_2023"
 
 extensions = [
     # there may be others here already, e.g. 'sphinx.ext.mathjax'
@@ -65,7 +65,7 @@ extensions = [
 
 # Setup the breathe extension
 breathe_projects = {
-    "taproot-template-project": "./doxyoutput/xml"
+    "Legacy_2023": "./doxyoutput/xml"
 }
 
 # Setup the exhale extension
